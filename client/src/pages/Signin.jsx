2a42156908d43bfc9922d5ee447d1ab935b04b1e
Krishna2610 <krishna.nanda99@gmail.com>
2024-05-11 21:47:55 +0530

    Add redux persist

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -2,13 +2,19 @@
 
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { signInStart,signInFailure,signInSucess } from '../redux/user/userSlice';
+
+
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const {loading,error}=useSelector((state)=>state.user)
   const navigate = useNavigate();
-  
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -19,7 +25,8 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(signInStart())
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -29,14 +36,18 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (!res.ok) {
-        throw new Error(data.message || 'Failed to sign up');
+        // throw new Error(data.message || 'Failed to sign up');
+        dispatch(signInFailure(data.message))
+        return
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSucess(data))
       navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(signInFailure(error.message))
     }
   };
   
