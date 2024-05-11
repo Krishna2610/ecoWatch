2b78dcc6b88b9f4cb964f8b467054c9c99e5caec
Krishna2610 <krishna.nanda99@gmail.com>
2024-05-11 18:29:08 +0530

    sign up func

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -85,3 +85,92 @@ export default function SignUp() {
     </div>
   );
 }
+
+// import { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+
+// export default function SignUp() {
+//   const [formData, setFormData] = useState({});
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const navigate = useNavigate();
+
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.id]: e.target.value,
+//     });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       setLoading(true);
+//       const res = await fetch('/api/auth/signup', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });
+//       const data = await res.json();
+//       console.log(data);
+//       if (data.success === false) {
+//         setLoading(false);
+//         setError(data.message);
+//       } else {
+//         setLoading(false);
+//         setError(null);
+//         // Only navigate if there are no errors
+//         if (!error) {
+//           navigate('/sign-in');
+//         }
+//       }
+//     } catch (error) {
+//       setLoading(false);
+//       setError(error.message);
+//     }
+//   };
+
+//   return (
+//     <div className='p-3 max-w-lg mx-auto'>
+//       <h1 className='text-3xl text-center font-bold font-custom my-7'>Sign Up</h1>
+//       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+//         <input
+//           type='text'
+//           placeholder='username'
+//           className='border-4 border-tealc focus:outline-none p-3 rounded-lg placeholder-black'
+//           id='username'
+//           onChange={handleChange}
+//         />
+//         <input
+//           type='email'
+//           placeholder='email'
+//           className='border-4 border-tealc focus:outline-none p-3 rounded-lg placeholder-black'
+//           id='email'
+//           onChange={handleChange}
+//         />
+//         <input
+//           type='password'
+//           placeholder='password'
+//           className='border-4 border-tealc focus:outline-none p-3 rounded-lg placeholder-black'
+//           id='password'
+//           onChange={handleChange}
+//         />
+//         <button
+//           disabled={loading}
+//           className='bg-orangec text-black font-extrabold  p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+//         >
+//           {loading ? 'Loading...' : 'Sign Up'}
+//         </button>
+//       </form>
+//       <div className='flex gap-2 mt-5'>
+//         <p>Already have an account?</p>
+//         <Link to={'/sign-in'}>
+//           <span className='text-blue-700 text-bold'>Sign in</span>
+//         </Link>
+//       </div>
+//       {error && <p className='text-red-500 mt-5'>{error}</p>}
+//     </div>
+//   );
+// }
