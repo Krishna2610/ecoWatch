b6c7f5eb6a41426082fa27d13b80ab09e4bde984
Krishna2610 <krishna.nanda99@gmail.com>
2024-05-12 11:58:55 +0530

    signout functtionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const google = async (req, res, next) => {
 export const signout = async (req, res, next) => {
     try {
 
-            res.clearCookie('access_token')
+            res.clearCookie('access_token');
             res.status(200).json("User has been logged out!!");
         }
     catch (error) {
